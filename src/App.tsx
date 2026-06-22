8543798496212a300b18119821681938ee0dec5f
PenguinKKIM <clfrck124@gmail.com>
2026-06-22 17:27:09 +0900

    feat : vite 설정&기본세팅

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Overlay from './assets/Overlay';
+
+function App() {
+
+  return (
+  <div>테스트 화면 입니다
+    <Overlay/>
+  </div>
+  )
+}
+
+export default App
---
+++ src/assets/Overlay.tsx
@@ -0,0 +1,9 @@
+const Overlay = () => {
+  return (
+    <div>
+      오버레이 페이지
+    </div>
+  );
+}
+
+export default Overlay;
