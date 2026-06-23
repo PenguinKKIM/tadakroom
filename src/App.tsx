6f714cc9037d9d89c027bb4d7979e9a77d7a6a7e
PenguinKKIM <clfrck124@gmail.com>
2026-06-23 19:33:05 +0900

    feat : 손 왔다갔다 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css'
 import Overlay from './pages/Overlay';
 
 function App() {
-  const [lastKey, setLastKey] = useState("");
-
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      console.log("키 눌림:", event.key);
-      setLastKey(event.key);
-    };
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
-  return (
-  <div>
-    테스트 화면 입니다
-    <p>마지막으로 누른 키: {lastKey}</p>
-    <Overlay/>
-  </div>
-  )
+return(
+  <Overlay/>
+)
 }
 
 export default App
